e50b3441db54dabff46daa2318dae6b79f3ad6af
Ricky Ratcliff <ricky0ratcliff@gmail.com>
2016-08-30 11:22:56 -0700

    first commit

---
+++ app/config/routes.py
@@ -0,0 +1,13 @@
+
+from system.core.router import routes
+
+
+routes['default_controller'] = 'Welcome'
+routes['POST']['/register'] = 'Welcome#register'
+routes['POST']['/login'] = 'Welcome#login'
+routes['POST']['/logout'] = 'Welcome#logout' 
+
+
+
+
+
