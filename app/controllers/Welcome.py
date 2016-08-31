97a3e54ab267f0fa30e23ca3d6d3a5c3b9de9ada
Amandeep Matharu <kaurdeepm0@gmail.com>
2016-08-31 15:44:53 -0700

    bcrypt

--- app/controllers/Welcome.py
+++ app/controllers/Welcome.py
@@ -3,52 +3,51 @@ from system.core.controller import *
 import sys
 
 class Welcome(Controller):
-    def __init__(self, action):
-        super(Welcome, self).__init__(action)
-       
-        self.load_model('WelcomeModel')
-        self.db = self._app.db
+	def __init__(self, action):
+		super(Welcome, self).__init__(action)
+	   
+		self.load_model('WelcomeModel')
+		self.db = self._app.db
 
    
-    def index(self):
-        
-        return self.load_view('/welcome/landing.html')
+	def index(self):
+		
+		return self.load_view('/welcome/landing.html')
 
-    def register(self):
-        sys.stderr.write('Test1\n')
-        user_info = {
-            "email" : request.form['email'],
-            "password" : request.form['password'],
-            "phone" : request.form['phone']
-        }
-        sys.stderr.write('Test2\n')
-        user_id = self.models['WelcomeModel'].add_user(user_info)
-        sys.stderr.write('Test3\n')
-        session['id'] = user_id[0]['id']
+	def register(self):
+		sys.stderr.write('Test1\n')
+		user_info = {
+			"email" : request.form['email'],
+			"password" : request.form['password'],
+			"phone" : request.form['phone']
+		}
+		
+		user_id = self.models['WelcomeModel'].add_user(user_info)
+		
+		session['id'] = user_id['user']['id']
 
-        users = self.models['WelcomeModel'].get_users(user_id)
-        sys.stderr.write('Test4\n')
-        return self.load_view('/restaurants/dashboard.html', users=users)
+		# users = self.models['WelcomeModel'].get_users(user_id)
+		# sys.stderr.write('Test4\n')
+		return self.load_view('/restaurants/dashboard.html')
 
 
-    def login(self):
-        user_info = {
-            "email" : request.form['email'],
-            "password" : request.form['password']
-        }
+	def login(self):
+		user_info = {
+			"email" : request.form['email'],
+			"password" : request.form['password']
+		}
 
-        user_id = self.models['WelcomeModel'].login_user(user_info)
+		user_id = self.models['WelcomeModel'].login_user(user_info)
 
-        session['id'] = user_id[0]['id']
-        session['email'] = user_id[0]['email']
+		# session['id'] = user_id[0]['id']
 
-        users = self.models['WelcomeModel'].get_users(user_id)
+		# users = self.models['WelcomeModel'].get_users(user_id)
 
-        return self.load_view('/restaurants/dashboard.html', users=users)
+		return self.load_view('/restaurants/dashboard.html')
 
-    
+	
 
-    def logout(self):
-        session.clear()
-        return self.load_view('/welcome/landing.html')
+	def logout(self):
+		session.clear()
+		return self.load_view('/welcome/landing.html')
 
--- app/models/WelcomeModel.py
+++ app/models/WelcomeModel.py
@@ -1,28 +1,53 @@
 from __future__ import print_function
 from system.core.model import Model
 import sys
+from sqlalchemy.sql import text
 
 class WelcomeModel(Model):
-    def __init__(self):
-        super(WelcomeModel, self).__init__()
+	def __init__(self):
+		super(WelcomeModel, self).__init__()
    
-    def add_user(self, user):
-        sys.stderr.write('Test5\n')
-        query = "INSERT INTO users (email, password, phone, created_at, updated_at) VALUES (:email, :password, :phone, NOW(), NOW())"
-        self.db.query_db(query, user)
-        sys.stderr.write('Test6\n')
-        query = 'SELECT * FROM users WHERE users.email = :email AND users.password = :password'
-        return self.db.query_db(query, user)
+	def add_user(self, user_info):
+		pw_hash = self.bcrypt.generate_password_hash(user_info['password'])
+		errors = []
+
+		sys.stderr.write('Test5\n')
+		query = "INSERT INTO users (email, password, phone, created_at, updated_at) VALUES (:email, :password, :phone, NOW(), NOW())"
+		user_data = {'email': user_info['email'],
+					 'password': pw_hash,
+					  'phone': user_info['phone'] 
+					  }
+		user = self.db.query_db(query, user_data)
+
+		sys.stderr.write('Test6\n')
+		query = 'SELECT * FROM users  WHERE users.email = :email AND users.password = :password'
+		users = self.db.query_db(query)
+		return { "status": True, "user": users[0] }
 
 
+	def login_user(self, user_info):
+		password = user_info['password']
+		errors = []
+		query ='SELECT * FROM users WHERE users.email = :email AND users.password = :password'
+		user_data = {
+				'email': user_info['email'],
+				'password': user_info['password']
+					}
+		user = self.db.get_one(query, user_data)
+		if user:
+			if self.bcrypt.check_password_hash(user.password, password):
+				return {"status": True, "user": user}
+		else:
+			errors.append('Invalid Login information')
+		# Whether we did not find the email, or if the password did not match, either way return False
+		return {"status": False, "errors": errors}
 
-    def login_user(self, user):
-
-        query ='SELECT * FROM users WHERE users.email = :email AND users.password = :password'
-        return self.db.query_db(query, user)
+		# return self.db.query_db(query, user)
 
 
-    def get_users(self, user):
-        query = 'SELECT * FROM users WHERE id = id'
-        data = {'id' : id}
-        return self.db.query_db(query)
+ 
+	   
+	def get_users(self, user):
+		query = 'SELECT * FROM users WHERE id = id'
+		data = {'id' : id}
+		return self.db.query_db(query)
