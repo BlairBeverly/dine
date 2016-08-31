b99cf83b84bb312e2acf2975775bf00c1cf6b4f3
Blair Beverly <blair.beverly@gmail.com>
2016-08-31 14:49:14 -0700

    enable filter by favorites

--- app/models/Restaurant.py
+++ app/models/Restaurant.py
@@ -6,22 +6,28 @@ class Restaurant(Model):
         super(Restaurant, self).__init__()
 
     def get_restaurants(self, page_num, score, favorites, user_id):
-   
-        query = "SELECT restaurant_id, name, address, city, state, "\
-        "postal_code, score, latitude, longitude, MAX(date) as date "\
-        "FROM restaurants r JOIN inspections i ON (r.id = i.restaurant_id) "\
-        "WHERE score <> '' AND score >= :score GROUP BY 1 "\
-        "LIMIT :exclude_until, 10 "
+
+        if favorites:
+            print favorites
+            query = "SELECT i.restaurant_id, name, address, city, state, "\
+            "postal_code, score, latitude, longitude, MAX(date) as date "\
+            "FROM restaurants r JOIN inspections i ON (r.id = i.restaurant_id) "\
+            "JOIN favorites f ON (f.restaurant_id = r.id) WHERE score <> '' "\
+            "AND score >= :score AND f.user_id = :user_id GROUP BY 1 "\
+            "LIMIT :exclude_until, 10 "
+
+        else:
+            query = "SELECT restaurant_id, name, address, city, state, "\
+            "postal_code, score, latitude, longitude, MAX(date) as date "\
+            "FROM restaurants r JOIN inspections i ON (r.id = i.restaurant_id) "\
+            "WHERE score <> '' AND score >= :score GROUP BY 1 "\
+            "LIMIT :exclude_until, 10 "
 
         data = {'exclude_until': int(page_num)*10,
-                'score': score}
+                'score': score,
+                'user_id': user_id}
 
         return self.db.query_db(query, data)
 
-    def get_fav_restaurants(self, user_id):
-        query = "SELECT restaurant_id FROM favorites WHERE user_id = :user_id"
-
-        data = {'user_id': user_id}
-
-        return self.db.query_db(query, data)
-
+    def get_restaurant_count(self):
+        pass
