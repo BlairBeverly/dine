8f9029f9186e2a2ea7ef7bf88da5e64021bda672
Blair Beverly <blair.beverly@gmail.com>
2016-09-01 11:34:23 -0700

    add/remove favorites

--- app/config/routes.py
+++ app/config/routes.py
@@ -8,3 +8,6 @@ routes['POST']['/login'] = 'Welcome#login'
 routes['POST']['/logout'] = 'Welcome#logout'
 routes['GET']['/restaurants/<page_num>'] = 'Restaurants#index'
 routes['POST']['/restaurants/filter'] = 'Restaurants#filter'
+routes['POST']['/restaurants/add_favorite/<user_id>/<restaurant_id>'] = 'Restaurants#add_favorite'
+routes['POST']['/restaurants/remove_favorite/<user_id>/<restaurant_id>'] = 'Restaurants#remove_favorite'
+
